e17295484cf7138a3e26398b1dbfee2c2d26253f
Tomas36M <tomasmuenvar36@gmail.com>
2021-10-23 19:50:56 -0500

    responsive disgn

--- Proyectos/Proyectos React/Proyecto Final/e-commerce/src/component/Product/Products.js
+++ Proyectos/Proyectos React/Proyecto Final/e-commerce/src/component/Product/Products.js
@@ -1,17 +1,29 @@
 import * as React from 'react';
+import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
+// import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+// import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import accounting from 'accounting';
 import './products.css'
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
 
 const getImgUrl = (image) => {
     if (image === undefined) {
@@ -29,6 +41,7 @@ export default function Products({
     brand,
     image
 }) {
+
     return (
         <Card className="cards" sx={{ maxWidth: 345 }}>
             <CardHeader
@@ -52,19 +65,16 @@ export default function Products({
                 onError='https://bitsofco.de/content/images/2018/12/broken-1.png'
                 alt="Product"
             />
-            <CardContent className="text-content">
+            {/* <CardContent className="text-content">
                 <Typography variant="body2" color="text.secondary">
-                    {description}
+                    {'Brand: ' + brand}
                 </Typography>
-            </CardContent>
+            </CardContent> */}
             <CardActions disableSpacing>
-            <p>Price: {price + '$'}</p>
-                <IconButton aria-label="add to favorites">
+                <h3 className="price">{accounting.formatMoney(price)}</h3>
+                <ExpandMore>
                     <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                    <ShareIcon />
-                </IconButton>
+                </ExpandMore>
             </CardActions>
         </Card>
 
--- Proyectos/Proyectos React/Proyecto Final/e-commerce/src/views/Home.js
+++ Proyectos/Proyectos React/Proyecto Final/e-commerce/src/views/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import UseAxios from '../hooks/UseAxios'
 import Products from '../component/Product/Products';
+import { Grid } from '@mui/material';
 
 const Home = () => {
     const [products, setProducts] = useState([]);
@@ -44,22 +45,23 @@ const Home = () => {
                 </div>
             )}
 
-            {products && <div className="products">
-                {console.log(filteredProducts)}
-                {filteredProducts.map(product => {
-                    return (
-                        <Products
-                            key={product.id}
-                            name={product.product_name}
-                            price={product.price}
-                            image={product.image}
-                            description={product.description}
-                            brand={product.brand}
-                            category={product.category}
-                        />
-                    )
-                })}
-            </div>
+            {products && <Grid className="products" container spacing={3}>
+                    {filteredProducts.map(product => {
+                        return (
+                            <Grid item xs={12} sm={6} md={4} lg={3}>
+                            <Products
+                                key={product.id}
+                                name={product.product_name}
+                                price={product.price}
+                                image={product.image}
+                                description={product.description}
+                                brand={product.brand}
+                                category={product.category}
+                            />
+                            </Grid>
+                        )
+                    })}
+            </Grid>
             }
         </div>
     )
